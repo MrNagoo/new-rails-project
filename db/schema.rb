d1fff03a66d96d9e18eb295b6f0f6dea373d6078
Aaron Nagucki <aaron.nagucki@gmail.com>
2016-01-31 20:55:04 -0500

    adds basic models

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160128205322) do
+
+  create_table "items", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "list_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "lists", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
